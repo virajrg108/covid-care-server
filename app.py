e4aaa66fe0147ba3dd6c251e398aec44c8a2f531
darthwaydr007 <hardityathebest@gmail.com>
2020-09-26 15:12:39 +0530

    model added

--- app.py
+++ app.py
@@ -14,21 +14,21 @@ def index():
 
 @app.route('/login', methods=['POST'])
 def login():
-	print(request.data)
-	d = request.data.decode('utf8').replace("'", '"')
-	print(d)
-	return make_response(jsonify(d), 200)
+    print(request.data)
+    d = request.data.decode('utf8').replace("'", '"')
+    print(d)
+    return make_response(jsonify(d), 200)
 
 
 @app.route("/get_patient_first_data", methods=["GET", "POST"])
 def take_input():
-	if request.method == 'POST':
-		data = request.get_json()
-		outcome = model.predict(data)
-	# if text is None:
-	#    return jsonify({"message":"text not found"})
+    if request.method == 'POST':
+        data = request.get_json()
+        outcome = model.predict(data)
+    # if text is None:
+    #    return jsonify({"message":"text not found"})
 
-	return jsonify({"Testing_required": outcome})
+    return jsonify({"Testing_required": outcome})
 
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -1,4 +1,14 @@
 def predict(data):
-    print(data['Name'])
-    print(data)
-    return "YES"
+    fever = data["Fever"]
+    sb = data["Short_breathe"]
+    cough = data["Cough"]
+    weakness = data["Weakness"]
+    travel = data["Travelled"]
+    cc = data["Close_Contact_covid"]
+    pne = data["Pneumonia"]
+    if (fever and
+        (sb or weakness or cough) and
+            (close_contact or exposure)) or pnemonia = Test:
+        return 1
+    else:
+        return 0
