2dce0d30815f159968458e3e8bfb1179c27c48e7
darthwaydr007 <hardityathebest@gmail.com>
2020-10-02 14:24:09 +0530

    model-added

--- model.py
+++ model.py
@@ -1,3 +1,7 @@
+import pickle
+import numpy as np
+
+
 def predict(data):
     fever = data["fever"]
     sb = data["breathe"]
@@ -6,7 +10,17 @@ def predict(data):
     travel = data["travelled"]
     close_contact = data["contact"]
     pneumonia = data["pneumonia"]
-    if (fever and (sb or weakness or cough) and (close_contact or pneumonia)):
-        return True
-    else:
-        return False
+    symptoms = []
+    symptoms.extend([fever, sb, cough, weakness, travel,
+                     close_contact, pneumonia])
+    pkl_filename = "model-covid.pkl"
+    with open(pkl_filename, 'rb') as file:
+        covid_classifier_pkl = pickle.load(file)
+    arr_symptoms = np.array(symptoms)
+    prediction = covid_classifier_pkl.predict(
+        arr_neg.reshape(-1, len(symptoms)))
+    return prediction
+    # if (fever and (sb or weakness or cough) and (close_contact or pneumonia)):
+    #    return True
+    # else:
+    #    return False
